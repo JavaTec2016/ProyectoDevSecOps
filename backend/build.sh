f39879161c527b817964803c1974207496898f30
Santiago Banuelos <santiago.bt@tecjerez.edu.mx>
2026-09-10 09:56:23 -0600

    arreglar el .env, BD, prod_origin, migracion django

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-# Exit on error
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
-python manage.py collectstatic --no-input
-
+#!/bin/bash
 # Apply any outstanding database migrations
 python manage.py migrate
--- backend/colectas_api/settings.py
+++ backend/colectas_api/settings.py
@@ -146,6 +146,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://192.168.1.8:5173",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
     os.getenv('PROD_ORIGIN', 'https://examplo.com'),
 ]
 CORS_ALLOW_CREDENTIALS = True
